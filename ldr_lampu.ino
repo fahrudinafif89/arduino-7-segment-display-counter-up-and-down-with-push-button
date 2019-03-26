1c890d8b4e0ad6c88ec51bade7a090bc9e40b587
fahrudinafif89 <48027602+fahrudinafif89@users.noreply.github.com>
2019-03-27 01:27:09 +0700

    Add files via upload

---
+++ ldr_lampu.ino
@@ -0,0 +1,26 @@
+/********************************************************************************************************************************
+ * ****8****dibuat oleh:https://github.com/fahrudinafif89/arduino-7-segment-display-counter-up-and-down-with-push-button*********
+ * ******************************************************************************************************************************
+ */
+byte ldr= A2;
+byte led= 13;
+int nilai;
+ 
+void setup(){
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+ 
+void loop(){
+  nilai= analogRead(ldr);
+  Serial.print("Nilai LDR: ");
+  Serial.println(nilai);
+ 
+  if(nilai < 500){
+    digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(led, LOW);
+  }
+  
+}
